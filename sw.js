58baec23a7c815bccbb7a6a959c58e8d09664c98
tkagiha <nniippppoonnnn@gmail.com>
2026-07-15 19:21:32 +0700

    tweak(jelajah): hold the city name reveal ~3.2s (was 1.8s)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Distinksi service worker v2 — 更新反映＋オフライン両立 */
-const CACHE = "distinksi-v60";
+const CACHE = "distinksi-v61";
 const SHELL = [
   "./", "./index.html", "./data.js", "./gloss.js", "./cards.js", "./extra.js",
   "./manifest.webmanifest", "./icon-192.png", "./icon-512.png", "./favicon.png", "./archipelago.png", "./news.js",
